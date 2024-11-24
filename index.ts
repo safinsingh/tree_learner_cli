26bc7d33cd351f6819cd1214133ae0e46cad64e6
Safin Singh <safin.singh@gmail.com>
2024-11-24 15:04:19 -0800

    prepare/build pkg

--- index.ts
+++ index.ts
@@ -1,15 +1,3 @@
-import { explore } from "./mindTree";
-import { formatMindTree } from "./semanticFormatter";
-import { ConsoleMindTreeWriter } from "./writer";
-
-const options = {
-	topic: "Topology",
-	depth: 3,
-	breadth: 3,
-};
-
-const mindTree = await explore(options);
-const formattedMindTree = formatMindTree(mindTree);
-const writer = new ConsoleMindTreeWriter(formattedMindTree);
-
-writer.write();
+export { explore, type MindTree, type ExploreOptions } from "./mindTree";
+export { formatMindTree, type FormattedMindTree } from "./semanticFormatter";
+export { ConsoleMindTreeWriter } from "./writer";
